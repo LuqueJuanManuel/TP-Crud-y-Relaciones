153a91bcd3436dc6b6597d79ac26f7402eb4f38d
JuanLuque <juanluquejml@gmail.com>
2023-04-24 16:15:36 -0300

    Trabajo practico Crud y relaciones

---
+++ src/Validations/moviesValidator.js
@@ -0,0 +1,10 @@
+const { check } = require('express-validator')
+
+module.exports = [
+ check('title').notEmpty().withMessage('Requerido'),
+ check('rating').notEmpty().withMessage('Requerido'),
+ check('awards').notEmpty().withMessage('Requerido'),
+ check('release_date').notEmpty().withMessage('Requerido'),
+ check("genre_id").notEmpty().withMessage("Genre Requerido")
+
+]
